f999668bd8443afe12ea2014b391db9119d9f9ee
Jasmine T <tanghocle456@gmail.com>
2023-08-22 01:17:05 -0700

    Added firebase credentials

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -14,21 +14,20 @@
 
 // export default firebaseApp;
 
-
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBIsJuu1QVaSy0rUdL_7rugdf5XvxybtHM",
+  authDomain: "ecoearnauth.firebaseapp.com",
+  projectId: "ecoearnauth",
+  storageBucket: "ecoearnauth.appspot.com",
+  messagingSenderId: "42057584434",
+  appId: "1:42057584434:web:38d9191cc4ed0b549bd12b",
+  measurementId: "G-C0DQTBG5MG"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth };
-
+export {auth};
