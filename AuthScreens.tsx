2b5263febbdf5ae70f1b5ff3df01882c99f92e27
Jasmine T <tanghocle456@gmail.com>
2023-08-22 08:50:25 -0700

    Add User name and Main address field in SignUpScreen

--- AuthScreens.tsx
+++ AuthScreens.tsx
@@ -20,6 +20,8 @@ type AuthScreenNavigationProp = StackNavigationProp<RootStackParamList, 'SignUp'
 export function SignUpScreen({ navigation }: { navigation: AuthScreenNavigationProp }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [mainAddress, setMainAddress] = useState('');
 
   const signUp = async () => {
     try {
@@ -36,8 +38,11 @@ export function SignUpScreen({ navigation }: { navigation: AuthScreenNavigationP
     <View>
       <TextInput placeholder="Email" onChangeText={text => setEmail(text)} />
       <TextInput placeholder="Password" onChangeText={text => setPassword(text)} secureTextEntry />
+      <TextInput placeholder='User name' onChangeText={text => setUsername(text)}/>
+      <TextInput placeholder='Main Address' onChangeText={text => setMainAddress(text)} />
       <Button title="Sign Up" onPress={signUp} />
       <Button title="Already have an account? Sign In" onPress={() => navigation.navigate('SignIn')} />
+    
     </View>
   );
 }
