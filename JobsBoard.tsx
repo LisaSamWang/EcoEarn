6fc6da3c9c06fc735634b50af2662602999fc0ca
Jasmine T <tanghocle456@gmail.com>
2023-08-23 18:06:13 -0700

    New branch, change button icons and change border color for jobs

--- JobsBoard.tsx
+++ JobsBoard.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, Alert} from 'react-native';
+import { View, Text, Button, FlatList, Alert } from 'react-native';
 import { db } from './firebaseConfig';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { firebase } from '@react-native-firebase/firestore';
@@ -17,7 +17,7 @@ export function JobsBoardScreen({ navigation }: { navigation: any }) {
     });
   }, [navigation]);
 
-  type Job = { id: string; jobPoster?: string; jobPosterEmail?: string; address?: string; numItems?: number; jobClaimer?: string; jobClaimerEmail?:string; completed?: boolean };
+  type Job = { id: string; jobPoster?: string; jobPosterEmail?: string; address?: string; numItems?: number; jobClaimer?: string; jobClaimerEmail?: string; completed?: boolean };
   const [jobs, setJobs] = useState<Job[]>([]);
   const [currentUserEmail, setCurrentUserEmail] = useState<string>("");
   const [currentUsername, setCurrentUsername] = useState<string | undefined>(undefined);
@@ -54,23 +54,23 @@ export function JobsBoardScreen({ navigation }: { navigation: any }) {
           }
         });
     }
-}, [currentUserEmail]);
+  }, [currentUserEmail]);
 
 
-    const checkProfileComplete = async (email: string) => {
-      const userDocRef = db.collection('users').doc(email);
-      const userDocSnap = await userDocRef.get();
-      const userData = userDocSnap.data();
+  const checkProfileComplete = async (email: string) => {
+    const userDocRef = db.collection('users').doc(email);
+    const userDocSnap = await userDocRef.get();
+    const userData = userDocSnap.data();
 
-      return (userData?.username && userData?.address);
-    };
+    return (userData?.username && userData?.address);
+  };
 
-    const claimJob = async (jobId: string) => {
-      if (!await checkProfileComplete(currentUserEmail)) {
-        Alert.alert("Please complete your profile before claiming a job.");
-        return;
-      }
-    
+  const claimJob = async (jobId: string) => {
+    if (!await checkProfileComplete(currentUserEmail)) {
+      Alert.alert("Please complete your profile before claiming a job.");
+      return;
+    }
+
     if (!currentUserEmail) return;
     const jobDocRef = db.collection('jobs').doc(jobId);
     await jobDocRef.update({ jobClaimer: currentUsername, jobClaimerEmail: currentUserEmail });
@@ -87,7 +87,7 @@ export function JobsBoardScreen({ navigation }: { navigation: any }) {
     // Update points for both the job claimer and the job poster
     const claimerDocRef = db.collection('users').doc(currentUserEmail);
     const posterDocRef = db.collection('users').doc(jobPoster);
-    
+
     await claimerDocRef.update({ points: firebase.firestore.FieldValue.increment(numItems * 2) });
     await posterDocRef.update({ points: firebase.firestore.FieldValue.increment(numItems) });
   };
@@ -97,22 +97,22 @@ export function JobsBoardScreen({ navigation }: { navigation: any }) {
       <FlatList
         data={jobs}
         renderItem={({ item }) => (
-          <View style={{ marginVertical: 10, padding: 10, borderColor: 'gray', borderWidth: 1}}>
-            <Text style={{color: 'black'}}>User: {item.jobPoster}</Text>
-            <Text style={{color: 'black'}}>Address: {item.address}</Text>
-            <Text style={{color: 'black'}}>Items to pick up: {item.numItems}</Text>
-            
-            {item.completed ? 
-              <Text style={{color: 'black'}}>Job Completed</Text> :
-              item.jobClaimer ? 
-              item.jobClaimerEmail === currentUserEmail ? 
-              <Button title="Completed" onPress={() => {
-                if (item.id && item.jobPosterEmail && item.numItems !== undefined) {
-                  completeJob(item.id, item.jobPosterEmail, item.numItems);
-                }
-              }} /> :
-              <Text style={{color: 'black'}}>Claimed by: {item.jobClaimer}</Text> : 
-              <Button title="Claim Job" onPress={() => claimJob(item.id)} />
+          <View style={{ marginVertical: 10, padding: 10, borderColor: 'forestgreen', borderWidth: 1 }}>
+            <Text style={{ color: 'black' }}>User: {item.jobPoster}</Text>
+            <Text style={{ color: 'black' }}>Address: {item.address}</Text>
+            <Text style={{ color: 'black' }}>Items to pick up: {item.numItems}</Text>
+
+            {item.completed ?
+              <Text style={{ fontStyle: 'italic', color: 'black' }}>Job Completed</Text> :
+              item.jobClaimer ?
+                item.jobClaimerEmail === currentUserEmail ?
+                  <Button title="Completed" onPress={() => {
+                    if (item.id && item.jobPosterEmail && item.numItems !== undefined) {
+                      completeJob(item.id, item.jobPosterEmail, item.numItems);
+                    }
+                  }} /> :
+                  <Text style={{ color: 'black' }}>Claimed by: {item.jobClaimer}</Text> :
+                <Button title="Claim Job" onPress={() => claimJob(item.id)} />
             }
           </View>
         )}
